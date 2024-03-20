c6bfab01e3152ebab05b936314918da36b275bbf
Corentin Charmeux <charmeux.corentin.pro@gmail.com>
2024-03-20 12:46:57 +0100

    feat: Ajout du texte qui change dans le carrousel + fix: Optimisation du code

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,62 @@
 const slides = [
     {
         "image": "assets/images/slideshow/slide1.jpg",
-        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+        "tagLine": "<p>Impressions tous formats <span>en boutique et en ligne</span></p>"
     },
     {
         "image": "assets/images/slideshow/slide2.jpg",
-        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+        "tagLine": "<p>Tirages haute définition grand format <span>pour vos bureaux et events</span></p>"
     },
     {
         "image": "assets/images/slideshow/slide3.jpg",
-        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+        "tagLine": "<p>Grand choix de couleurs <span>de CMJN aux pantones</span></p>"
     },
     {
         "image": "assets/images/slideshow/slide4.png",
-        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+        "tagLine": "<p>Autocollants <span>avec découpe laser sur mesure</span></p>"
     }
 ];
 
-
-
-let actualImage = 0;
-function changeImage(side) {
+// Fonction qui permet de changé l'image, le texte et le dot.
+let actualChange = 0;
+function changeElement(side) {
+    // Récupération des éléments par l'id.
     let imagesPages = document.getElementById("images");
+    let list_dot = document.querySelectorAll(".dot");
+    let text = document.getElementById("textImages")
+    // Vérification si c'est "right" ou "left".
     if (side === "right") {
-        actualImage++;
-        if (actualImage > 3) {
-            actualImage = 0;
+        list_dot[actualChange].classList.remove('dot_selected');
+        actualChange++;
+        if (actualChange > 3) {
+            actualChange = 0;
         };
-        imagesPages.setAttribute("src", slides[actualImage].image);
+        imagesPages.setAttribute("src", slides[actualChange].image);
+        list_dot[actualChange].classList.add('dot_selected');
+        text.innerHTML = slides[actualChange].tagLine
     } else if (side === "left") {
-        actualImage--;
-        if (actualImage < 0) {
-            actualImage = 3;
+        list_dot[actualChange].classList.remove('dot_selected');
+        actualChange--;
+        if (actualChange < 0) {
+            actualChange = 3;
         };
-        imagesPages.setAttribute("src", slides[actualImage].image);
+        imagesPages.setAttribute("src", slides[actualChange].image);
+        list_dot[actualChange].classList.add('dot_selected');
+        text.innerHTML = slides[actualChange].tagLine
     }
 }
-
-let currentDot = 0;
-function changeDotClass(side) {
-    let list_dot = document.querySelectorAll(".dot");
-    if (side === "right") {
-        list_dot[currentDot].classList.remove('dot_selected');
-        currentDot++;
-        if (currentDot > 3) {
-            currentDot = 0;
-        };
-        list_dot[currentDot].classList.add('dot_selected');
-    } else if (side === "left") {
-        list_dot[currentDot].classList.remove('dot_selected');
-        currentDot--;
-        if (currentDot < 0) {
-            currentDot = 3;
-        };
-        list_dot[currentDot].classList.add('dot_selected');
-    };
-}
-
 
 const left_arrow = document.getElementById("arrow_left");
 const right_arrow = document.getElementById("arrow_right");
 
+// Ajout d'event au click sur la flèche de droite.
 right_arrow.addEventListener("click", () => {
     let side = "right"
-    changeImage(side) 
-    changeDotClass(side)   
+    changeElement(side)
 })
 
+// Ajout d'event au click sur la flèche de gauche.
 left_arrow.addEventListener("click", () => {
     let side = "left"
-    changeImage(side)
-    changeDotClass(side)
+    changeElement(side)
 })
